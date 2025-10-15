c67fa63081ef8e0f8ec3df487e8665386aaf1b3d
Othniel Gnimassou <122475519+cheznousdevgroup@users.noreply.github.com>
2025-10-15 09:29:59 +0000

    add changes

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+def main():
+    load_dotenv()
+    
+    print("="*60)
+    print("  Outil de Scraping Web - Locanto")
+    print("="*60)
+    
+    
+    print("\n Projet installé avec succès!")
+
+if __name__ == "__main__":
+    main()
